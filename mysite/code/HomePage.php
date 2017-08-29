829a0caf2a6a73156178fdabd768aecfbd20131c
fatwreck <dalepidlisny@gmail.com>
2017-08-29 07:57:35 -0600

    Removed homepage sort.

--- mysite/code/HomePage.php
+++ mysite/code/HomePage.php
@@ -23,7 +23,7 @@ class HomePage_Controller extends Page_controller {
 
 	public function LatestArticles($count = 6) {
 		return ArticlePage::get()
-			->sort('Created', 'DESC')
+			// ->sort('Created', 'DESC')
 			->limit($count);
 	}
 
